5ac508e2ab78e6899dd9a6aceb409f788d31f800
Michal Krasucki <266082+kmichal@users.noreply.github.com>
2026-02-04 22:51:17 -0500

    implement command-line options

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <CLI/CLI.hpp>
+
 
 int main(int argc, char** argv) {
+    
+    std::string token;
+    std::string symbol;
+    int lookback_days;
+    int days_forward;
+    int simulations;
 
-    std::cout << "Monte Carlo Simulation Tool\n";
+    CLI::App app{"Monte Carlo Simulation Tool"};
+
+    app.add_option("--tradier-token", token, "Tradier API token")->envname("TRADIER_API_TOKEN")->required();
+    app.add_option("--symbol", symbol, "Ticker symbol")->required();
+    app.add_option("--lookback-days", lookback_days, "Number of trading days to look back")->default_val(90);
+    app.add_option("--days-forward", days_forward, "Number of trading days to look forward")->default_val(90);
+    app.add_option("--simulations", simulations, "Number of simulations to run")->default_val(10000);
+
+    CLI11_PARSE(app, argc, argv);
+
+
+
+
     return 0;
 
 }
